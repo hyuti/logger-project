2e2ce37d2456f46c6e2eab9e066e0aab50884397
tsolution <lehuy.hl27@gmail.com>
2022-12-20 17:01:13 +0700

    add migration

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,9 +9,11 @@ import (
 	"github.com/TcMits/ent-clean-template/internal/collection"
 	v1 "github.com/TcMits/ent-clean-template/internal/controller/http/v1"
 	"github.com/TcMits/ent-clean-template/internal/hook"
+	_ "github.com/TcMits/ent-clean-template/migrations"
 	"github.com/pocketbase/pocketbase/core"
 
 	"github.com/TcMits/ent-clean-template/pkg/infrastructure/logger"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 // Run creates objects via constructors.
@@ -21,6 +23,9 @@ func Run(cfg *config.Config) {
 	handler := v1.NewHandler()
 
 	cmd.RegisterCMD(handler, l, cfg)
+	migratecmd.MustRegister(handler, handler.RootCmd, &migratecmd.Options{
+		Automigrate: true,
+	})
 	handler.OnRecordBeforeCreateRequest().Add(func(e *core.RecordCreateEvent) error {
 		return collection.Validate(e.HttpContext, e.Record, l, cfg)
 	})
