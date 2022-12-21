952c7d971f6e60a9c97b05b6812dd85c81d28c8d
tsolution <lehuy.hl27@gmail.com>
2022-12-21 17:17:34 +0700

    update task

--- internal/controller/cron/cron.go
+++ internal/controller/cron/cron.go
@@ -9,24 +9,25 @@ import (
 	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/models"
+	"github.com/pocketbase/pocketbase/tools/types"
 )
 
 func NewScheduler() *gocron.Scheduler {
-  return gocron.NewScheduler(time.UTC)
+	return gocron.NewScheduler(time.UTC)
 }
 
-func registerHook(s *gocron.Scheduler, handler core.App, projName string, amountToDel int, l logger.Interface, cfg *config.Config) {
+func registerHook(s *gocron.Scheduler, handler core.App, projName string, l logger.Interface, cfg *config.Config) {
 	startAfter1Hour := time.Now().UTC().Add(time.Hour)
 
 	s.Every(cfg.Schedule.Period).Seconds().StartAt(startAfter1Hour).Do(func() {
 		l.Info("Running %s task...", projName)
 
+		now := time.Now().UTC().Add(-time.Hour).Format(types.DefaultDateLayout)
 		collection, _ := handler.Dao().FindCollectionByNameOrId(projName)
 		query := handler.Dao().RecordQuery(collection).
-			Where(dbx.NotBetween("status", 500, 599)).
-			OrderBy("created ASC").
-			Limit(int64(amountToDel))
-
+			AndWhere(dbx.NotBetween("status", 500, 599)).
+			AndWhere(dbx.NewExp("created <= {:date}", dbx.Params{"date": now})).
+			OrderBy("created ASC")
 		rows := []dbx.NullStringMap{}
 		if err := query.All(&rows); err != nil {
 			l.Fatal("deleting %s logs failed due to %s", projName, err)
@@ -43,7 +44,7 @@ func registerHook(s *gocron.Scheduler, handler core.App, projName string, amount
 }
 
 func RegisterHook(scheduler *gocron.Scheduler, handler core.App, l logger.Interface, cfg *config.Config) {
-	registerHook(scheduler, handler, cfg.TheHillAdminProject.Name, cfg.TheHillAdminProject.AmountToDelete, l, cfg)
-	registerHook(scheduler, handler, cfg.TheHillCustomerProject.Name, cfg.TheHillCustomerProject.AmountToDelete, l, cfg)
-	registerHook(scheduler, handler, cfg.TheHillStoreProject.Name, cfg.TheHillStoreProject.AmountToDelete, l, cfg)
+	registerHook(scheduler, handler, cfg.TheHillAdminProject.Name, l, cfg)
+	registerHook(scheduler, handler, cfg.TheHillCustomerProject.Name, l, cfg)
+	registerHook(scheduler, handler, cfg.TheHillStoreProject.Name, l, cfg)
 }
